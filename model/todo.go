9a808dab70798af757dc2dad0181edbeb2ab2fd1
Aditya Raj <adityaraj93107@gmail.com>
2026-03-22 17:30:03 +0530

    add Todo model with status constants

---
+++ model/todo.go
@@ -0,0 +1,16 @@
+package model
+
+import "time"
+
+const (
+    StatusTodo       = "todo"
+    StatusInProgress = "in-progress"
+    StatusDone       = "done"
+)
+
+type Todo struct {
+	ID        string    	`json:"id"`
+	Title     string 	`json:"title"`
+	Status    string 	`json:"status"`  
+	CreatedAt time.Time	`json:"created_at"`
+}
